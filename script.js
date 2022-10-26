c1c6b7300f654d87ccb1a07bd86557f06f541e59
Praddyota Hayaran <94554048+praddyotahayaran@users.noreply.github.com>
2022-10-26 09:31:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-const formEl = document.querySelector(".form")
-const inputEl = document.querySelector(".input")
-const ulEl = document.querySelector(".list")
-let list = JSON.parse(localStorage.getItem("list"))
+const formEl = document.querySelector(".form");
+const inputEl = document.querySelector(".input");
+const ulEl = document.querySelector(".list");
+let list = JSON.parse(localStorage.getItem("list"));
 
 list.forEach(task=>{
-    todoList(task)
+    todoList(task);
 })
 
 formEl.addEventListener("submit", (event) => {
