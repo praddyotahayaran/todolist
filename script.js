3639feeaaa051e3024f7cba06589bf6facc49bcb
Praddyota Hayaran <94554048+praddyotahayaran@users.noreply.github.com>
2022-10-26 09:35:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const formEl = document.querySelector(".form");
 const inputEl = document.querySelector(".input");
 const ulEl = document.querySelector(".list");
 let list = JSON.parse(localStorage.getItem("list"));
+let task = undefined;
 
 list.forEach(task=>{
     todoList(task);
